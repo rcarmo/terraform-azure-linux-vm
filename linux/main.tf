83f80edad79eaa8f2b30c09421fd58a07c62855e
Rui Carmo <rui.carmo@gmail.com>
2018-06-10 18:09:46 +0100

    Reinstante IP, NSG and cloud-init

--- linux/main.tf
+++ linux/main.tf
@@ -1,15 +1,81 @@
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.name}"
+resource "azurerm_network_security_group" "inbound" {
+  name                = "${var.name}-inbound"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "${var.ssh_port}"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = "${var.tags}"
+}
+
+resource "azurerm_public_ip" "linux" {
+  name                         = "${var.name}"
+  location                     = "${var.location}"
+  resource_group_name          = "${var.resource_group_name}"
+  public_ip_address_allocation = "Dynamic"
+  idle_timeout_in_minutes      = 30
+  domain_name_label            = "${var.resource_group_name}-${var.name}"
+
+  tags = "${var.tags}"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                      = "${var.name}"
+  location                  = "${var.location}"
+  resource_group_name       = "${var.resource_group_name}"
+  network_security_group_id = "${azurerm_network_security_group.inbound.id}"
 
   ip_configuration {
     name                          = "${var.name}-config"
     subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.linux.id}"
   }
 
   tags = "${var.tags}"
+}
+
+data "template_file" "cloud_config" {
+  template = "${file("${path.module}/cloud-config.yml.tpl")}"
+
+  vars {
+    ssh_port       = "${var.ssh_port}"
+    admin_username = "${var.admin_username}"
+  }
 }
 
 resource "azurerm_virtual_machine" "linux" {
@@ -40,8 +106,7 @@ resource "azurerm_virtual_machine" "linux" {
   os_profile {
     computer_name  = "${var.name}"
     admin_username = "${var.admin_username}"
-    custom_data    = <<CUSTOM_DATA
-CUSTOM_DATA
+    custom_data    = "${base64encode(data.template_file.cloud_config.rendered)}"
   }
 
   os_profile_linux_config {
@@ -149,8 +214,9 @@ resource "azurerm_virtual_machine_extension" "diagnostics" {
 
   settings           = "${data.template_file.settings.rendered}"
   protected_settings = "${data.template_file.protected_settings.rendered}"
+  tags               = "${var.tags}"
 }
 
-output "vm" {
+output "virtual_machine" {
   value = "${azurerm_virtual_machine.linux.id}"
 }
